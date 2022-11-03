da6e2759fb90003dd47da60951c475651ccbafd4
Oscar Javier Buitrago Saenz <buitrago22saenz@gmail.com>
2022-11-03 18:56:03 -0500

    primera publicacion

---
+++ seguridad 2/src/test/java/grupo/misiontic/seguridad/SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo.misiontic.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
